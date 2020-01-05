f94f568a0e8bcb3a365da8a05f303362d4e7d93d
tanakanaoko <nakochi.xxx@gmail.com>
2020-01-05 20:12:21 +0900

    tokyoを中心にセット

--- Uber/ViewController.swift
+++ Uber/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        mapView.delegate = self
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let location = CLLocationCoordinate2D(latitude: 35.689500, longitude: 139.6917100)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
     }
 
 
